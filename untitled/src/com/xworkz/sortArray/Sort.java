c34375524e71d4d53dd82f94b118c044681a892f
shashank007 <shashankmn066@gmail.com>
2025-04-24 20:50:27 +0530

    "uploading sorting"

---
+++ untitled/src/com/xworkz/sortArray/Sort.java
@@ -0,0 +1,15 @@
+package com.xworkz.sortArray;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Sort {
+    public static void main(String[] args) {
+        Integer[] arr = {10, 20 , 30, 40, 50, 60, 70, 80, 90, 99};
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(int number : arr){
+            System.out.print(" "+number);
+        }
+    }
+}
